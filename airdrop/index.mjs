e2453e5b11134c6390f499261fe3b5d6fc3621f6
Harry Ryder <hazryder@gmail.com>
2023-06-11 01:02:19 +0100

    Fixed config import, added vscode launch settings

--- airdrop/index.mjs
+++ airdrop/index.mjs
@@ -1,34 +1,46 @@
 import { AppWallet, BlockfrostProvider, Transaction } from "@meshsdk/core"
-import { BLOCKFROST_API_KEY, WALLET_SEED, TOKEN_ID, AMOUNT, RECIPIENTS } from "./config.json"
+import fs from "fs"
 
-const provider = new BlockfrostProvider(BLOCKFROST_API_KEY)
+async function main() {
+    if(!fs.existsSync("./config.json")) {
+        console.log("Missing config.json file")
 
-const wallet = new AppWallet({
-    networkId: 1,
-    fetcher: provider,
-    submitter: provider,
-    key: {
-        type: "mnemonic",
-        words: WALLET_SEED
+        return
     }
-})
-
-var tx = new Transaction({initiator: wallet})
-
-for(const recipient of RECIPIENTS) {
-    // Resolve ADA handles to addresses
-    const addr = recipient.substring(0,4).includes("addr1") ? recipient : await provider.fetchHandleAddress(recipient.replace("$",""))
-
-    tx = tx.sendAssets(addr, [
-        {
-            unit: TOKEN_ID,
-            quantity: AMOUNT
+    
+    const { BLOCKFROST_API_KEY, WALLET_SEED, TOKEN_ID, AMOUNT, RECIPIENTS } = JSON.parse(fs.readFileSync("./config.json"))
+    
+    const provider = new BlockfrostProvider(BLOCKFROST_API_KEY)
+    
+    const wallet = new AppWallet({
+        networkId: 1,
+        fetcher: provider,
+        submitter: provider,
+        key: {
+            type: "mnemonic",
+            words: WALLET_SEED
         }
-    ])
+    })
+    
+    var tx = new Transaction({initiator: wallet})
+    
+    for(const recipient of RECIPIENTS) {
+        // Resolve ADA handles to addresses
+        const addr = recipient.substring(0,5).includes("addr1") ? recipient : await provider.fetchHandleAddress(recipient.replace("$",""))
+    
+        tx = tx.sendAssets(addr, [
+            {
+                unit: TOKEN_ID,
+                quantity: AMOUNT
+            }
+        ])
+    }
+    
+    const unsignedTx = await tx.build()
+    
+    const signedTx = await wallet.signTx(unsignedTx)
+    
+    console.log(await wallet.submitTx(signedTx))
 }
 
-const unsignedTx = await tx.build()
-
-const signedTx = await wallet.signTx(unsignedTx)
-
-console.log(await wallet.submitTx(signedTx))
+main()
