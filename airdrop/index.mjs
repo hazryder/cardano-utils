df23b11d18adcc536aae6eb410519b811bd493ed
Harry Ryder <hazryder@gmail.com>
2023-06-12 10:43:07 +0100

    Added support for sending multiple tokens per airdrop. Improved error handling.

--- airdrop/index.mjs
+++ airdrop/index.mjs
@@ -2,44 +2,43 @@ import { AppWallet, BlockfrostProvider, Transaction } from "@meshsdk/core"
 import fs from "fs"
 
 async function main() {
-    if(!fs.existsSync("./config.json")) {
+    if (!fs.existsSync("./config.json")) {
         console.log("Missing config.json file")
 
         return
     }
-    
-    const { BLOCKFROST_API_KEY, WALLET_SEED, TOKEN_ID, AMOUNT, RECIPIENTS } = JSON.parse(fs.readFileSync("./config.json"))
-    
-    const provider = new BlockfrostProvider(BLOCKFROST_API_KEY)
-    
+
+    const { blockfrostApiKey, walletSeed, tokens, recipients } = JSON.parse(fs.readFileSync("./config.json"))
+
+    const provider = new BlockfrostProvider(blockfrostApiKey)
+
     const wallet = new AppWallet({
         networkId: 1,
         fetcher: provider,
         submitter: provider,
         key: {
             type: "mnemonic",
-            words: WALLET_SEED
+            words: walletSeed
         }
     })
-    
-    var tx = new Transaction({initiator: wallet})
-    
-    for(const recipient of RECIPIENTS) {
+
+    var tx = new Transaction({ initiator: wallet })
+
+    for (const recipient of recipients) {
         // Resolve ADA handles to addresses
-        const addr = recipient.substring(0,5).includes("addr1") ? recipient : await provider.fetchHandleAddress(recipient.replace("$",""))
-    
-        tx = tx.sendAssets(addr, [
-            {
-                unit: TOKEN_ID,
-                quantity: AMOUNT
-            }
-        ])
+        const addr = recipient.substring(0, 5).includes("addr1") ? recipient :
+            await provider.fetchHandleAddress(recipient.replace("$", ""))
+                .catch((e) => { console.log(`Addr not found for handle ${recipient}`, e) })
+
+        if (addr) {
+            tx = tx.sendAssets(addr, tokens)
+        }
     }
-    
+
     const unsignedTx = await tx.build()
-    
+
     const signedTx = await wallet.signTx(unsignedTx)
-    
+
     console.log(await wallet.submitTx(signedTx))
 }
 
